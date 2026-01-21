14b9f836233df5f9348991aef7840d36934d7924
yuri-mrs <162909475+yuri-mrs@users.noreply.github.com>
2026-01-20 22:07:24 -0300

    ✅ Up: Base dos Cards atualizada

--- portfolio/src/assets/components/cards/CardProjeto.tsx
+++ portfolio/src/assets/components/cards/CardProjeto.tsx
@@ -2,29 +2,29 @@ import { useState } from 'react'
 import '../../css/Main.css'
 
 function CardProjeto() {
-    const [count, setCount] = useState(0)
-
     return (
         <>
             <div>
                 <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                    <img src="https://placehold.co/300x200" className="logo" alt="Vite logo" />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h2>CardCursoInfo</h2>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-                </button>
+                <div>
+                    <button>
+                        Github
+                    </button>
+                    <button>
+                        Pagina
+                    </button>
+                </div>
                 <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
+                    
                 </p>
             </div>
             <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
+                descrição do curso
             </p>
         </>
     )
