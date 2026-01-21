68c135ecd921fe2255d385c0332cd435d641c66b
yuri-mrs <162909475+yuri-mrs@users.noreply.github.com>
2026-01-21 20:20:54 -0300

    💄Style: dimençoes de banner

--- portfolio/src/assets/components/banners/BannerTopo.tsx
+++ portfolio/src/assets/components/banners/BannerTopo.tsx
@@ -3,7 +3,9 @@ import '../../css/Main.css'
 
 function BannerTopo() {
     return (
-        <div class=" bg-cyan-700">
+        <div class="banner-out-1 bg-cyan-700">
+            <img src="" alt="" />
+            <p>Hello World !! hi i'm yuri</p>
         </div>
     )
 }
