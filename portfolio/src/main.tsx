7d511ec4672b0090b135a0d337b8e9ab0fc85d62
yuri-mrs <162909475+yuri-mrs@users.noreply.github.com>
2026-01-20 22:02:45 -0300

    ✅ Up: CardCurso layout, importaçoes no main

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/index.css'
-import App from './assets/components/App.tsx'
+
+import CardCurso from './assets/components/cards/CardCurso.tsx'
+import CardExperiencia from './assets/components/cards/CardExperiencia.tsx'
+import CardProjeto from './assets/components/cards/CardProjeto.tsx'
+
+import Menu-social from './assets/components/menus/Menu-social.tsx'
+import Menu-zonas from './assets/components/menus/Menu-zonas.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CardCurso />
   </StrictMode>,
 )
