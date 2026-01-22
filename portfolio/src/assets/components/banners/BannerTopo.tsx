5e248cefa1b4e1f9a6b0c4b93372798bba85f539
yuri-mrs <162909475+yuri-mrs@users.noreply.github.com>
2026-01-22 16:20:20 -0300

    ✨ Feat: Adcionado mais estrutura no banner

--- portfolio/src/assets/components/banners/BannerTopo.tsx
+++ portfolio/src/assets/components/banners/BannerTopo.tsx
@@ -4,6 +4,16 @@ import '../../css/Main.css'
 function BannerTopo() {
     return (
         <div class="banner-out-1 bg-cyan-700">
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
             <img src="" alt="" />
             <p>Hello World !! hi i'm yuri</p>
         </div>
