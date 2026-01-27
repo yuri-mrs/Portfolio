6bbc4e1e5017443d9e7c2dc440f936fb468dd207
yuri-mrs <162909475+yuri-mrs@users.noreply.github.com>
2026-01-27 14:48:03 -0300

    🔥 Burn: Remoção de trecho de codigo

--- portfolio/src/assets/components/cards/CardProjeto.tsx
+++ portfolio/src/assets/components/cards/CardProjeto.tsx
@@ -7,9 +7,7 @@ function CardProjeto() {
     return (
         <div className="card1 center-col bg-blue-700">
             <div className="center">
-                <a href="https://vite.dev" target="_blank">
-                    <img src="https://placehold.co/220x230" className="logo" alt="Vite logo" />
-                </a>
+                <img src="https://placehold.co/220x230" className="logo" alt="Vite logo" />
             </div>
             <h2 className="card-title" >CardProjeto</h2>
             <div className="center gap-5">
