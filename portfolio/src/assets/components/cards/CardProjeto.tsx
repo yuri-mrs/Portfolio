617883020bf0a377be0d8b2c1026342a6f25d269
yuri-mrs <162909475+yuri-mrs@users.noreply.github.com>
2026-01-27 15:26:53 -0300

    ✅ Up: Logica para multidados

--- portfolio/src/assets/components/cards/CardProjeto.tsx
+++ portfolio/src/assets/components/cards/CardProjeto.tsx
@@ -1,26 +1,22 @@
 import { useState } from 'react'
 import '../../css/Main.css'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import PublicIcon from '@mui/icons-material/Public';
 
-function CardProjeto() {
+function CardProjeto({ title, description, icon1: Icon1,icon2: Icon2 }) {
     return (
         <div className="card1 center-col border-8 border-double rounded-lg bg-teal-700">
             <div className="center">
                 <img src="https://placehold.co/220x230" className="logo" alt="Vite logo" />
             </div>
-            <h2 className="card-title" >CardProjeto</h2>
+            <h2 className="card-title" >{title}</h2>
             <div className="center gap-5">
                     <button className="buttom-card">
-                        <GitHubIcon fontSize="large"/>
+                        <Icon1/>
                     </button>
                     <button className="buttom-card">
-                        <PublicIcon fontSize="large"/>
+                        <Icon2/>
                     </button>
             </div>
-            <p className="center">
-                descrição do Projeto
-            </p>
+            <p className="center">{description}</p>
         </div>
     )
 }
