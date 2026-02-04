b19a0047c8046f5fd5dd24652cd6f94be1e13286
yuri-mrs <162909475+yuri-mrs@users.noreply.github.com>
2026-02-04 18:23:23 -0300

    🗑️ Clean: codigo problematico

--- portfolio/src/assets/components/cards/CardProjeto.tsx
+++ portfolio/src/assets/components/cards/CardProjeto.tsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
 import '../../css/Main.css'
 
-import ProjetoData from '../../data/CardProjetoData.ts'
-
-function CardProjeto({ title, description, icon1: Icon1,icon2: Icon2 }) {
+function CardProjeto() {
     return (
         <div className="card1 center-col border-8 border-double rounded-lg bg-teal-700">
             <div className="center">
                 <img src="https://placehold.co/220x230" className="logo" alt="Vite logo" />
             </div>
-            <h2 className="card-title" >{title}</h2>
+            <h2 className="card-title" ></h2>
             <div className="center gap-5">
                     <button className="buttom-card">
-                        <Icon1/>
+                        
                     </button>
                     <button className="buttom-card">
-                        <Icon2/>
+                        
                     </button>
             </div>
-            <p className="center">{description}</p>
+            <p className="center"></p>
         </div>
     )
 }
